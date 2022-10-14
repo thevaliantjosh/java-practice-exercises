273543f4802e1bbeeccd503726a9f08c6a7e4e0a
Joshua Sprouse <joshuacalebsprouse@gmail.com>
2022-10-14 11:35:23 -0500

    String Practice Problem 6

--- src/StringPractice.java
+++ src/StringPractice.java
@@ -1,5 +1,7 @@
 import org.w3c.dom.ls.LSOutput;
 
+import java.util.Scanner;
+
 public class StringPractice {
     public static void main(String[] args) {
         //Problem 1:
@@ -62,8 +64,49 @@ public class StringPractice {
         boolean eTwoIseFour = eTwo.equals(eFour);
         System.out.printf("%nComparing %s and %s: %b%nComparing %s and %s: %b%nComparing %s and %s: %b%nComparing %s and %s: %b%n", eOne, eTwo, eOneIseTwo, eOne, eThree, eOneIseThree, eTwo, eThree, eTwoIseThree, eTwo, eFour, eTwoIseFour);
 
-    }
+        //Problem 5
+        //Check whether a given String ends with the contents fo another string
+        boolean pythonEnding = python.endsWith("se");
+        boolean pythonEnding2 = python.endsWith("es");
+        System.out.printf("%n%s ends with \"se\"? %b%n%s ends with \"es\"? %b%n", python, pythonEnding, python, pythonEnding2);
 
+        //Problem 6
+        //Check whether a string equals another strings ignoring case considerations
+        Scanner in = new Scanner(System.in);
+        String theKing = "Stephen Edwin King";
+        String mrWinch = "Walter Winchell";
+
+
+
+
+//        String usersInput;
+//        usersInput = in.next().toLowerCase();
+//        boolean decline = usersInput.equals("n");
+//        if (decline) {
+//            break;
+//        }
+        System.out.println("Type in a String of the King or the Winch!");
+        while(true){
+            String usersInput = in.nextLine();
+            if(usersInput.equalsIgnoreCase(theKing)){
+                System.out.println("Hail to the king! That String be-ith equal! Continue? [y/N]");
+            } else if(usersInput.equalsIgnoreCase(mrWinch)){
+                System.out.println("Stop the presses! That's \"The Winch\"! Continue? [y/N]");
+            } else {
+                System.out.println("That doesn't match any string in the pool! Try again? [y/N]");
+            }
+            String dismiss;
+            dismiss = in.nextLine();
+            if(dismiss.equalsIgnoreCase("n")){
+                break;
+            }
+        }
+
+
+
+
+
+    }
 
 
 }
